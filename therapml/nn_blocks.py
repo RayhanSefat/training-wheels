c4db96a3e0bab061560e334ce775cea51db74008
Md. Rayhan Islam Sefat <rayhanbcpsc@gmail.com>
2026-02-17 09:50:37 +0600

    [task] swiglu

--- therapml/nn_blocks.py
+++ therapml/nn_blocks.py
@@ -22,5 +22,16 @@ def linear(d_in, d_out, weights, in_features):
     weights_transposed = weights.T
     return np.dot(in_features, weights_transposed)
 
+def SiLU(in_features):
+    data = np.asanyarray(in_features)
+    
+    return data / (1 + np.exp(-data))
+
 def swiglu(d_model, d_ff, w1_weight, w2_weight, w3_weight, in_features):
-    return in_features
+    gate = np.dot(in_features, w1_weight.T)
+    gate_activated = SiLU(gate)
+
+    value = np.dot(in_features, w3_weight.T)
+
+    output = gate_activated * value
+    return np.dot(output, w2_weight.T)
